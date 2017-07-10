cfa4ee45209c5c02e5360ed53476890c9827b9cd
Olivia <obrown917@gmail.com>
2017-07-10 16:22:32 -0700

    Fixed store storyboard

--- KittyJump/StoreViewController.swift
+++ KittyJump/StoreViewController.swift
@@ -12,10 +12,33 @@ class StoreViewController: UIViewController {
 
     @IBOutlet weak var currentCoins: UILabel!
     
+    @IBOutlet weak var startOver: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         currentCoins.text = "\(SharingManager.sharedInstance.lifetimeScore)"
+    }
+    
+    // Recognize if startOver image is tapped
+    override func viewDidAppear(_ animated: Bool) {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        startOver.isUserInteractionEnabled = true
+        startOver.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    // Unwind segue back to gameView
+    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        performSegue(withIdentifier: "unwindToHomeView", sender: self)
+    }
+    
+    // Replay game with unwind segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "unwindToHomeView" {
+            if let gameViewController = segue.destination as? GameViewController {
+                gameViewController.isReplayGame = true
+            }
+        }
     }
     
     // Hide status bar
