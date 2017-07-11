6a97973b209886e0700ca6804318302f88b0cad6
Olivia <obrown917@gmail.com>
2017-07-11 12:41:21 -0700

    Popups to confirm or buy coins

--- KittyJump/StoreViewController.swift
+++ KittyJump/StoreViewController.swift
@@ -11,8 +11,18 @@ import UIKit
 class StoreViewController: UIViewController {
     
     @IBOutlet weak var currentCoins: UILabel!
-    
+    @IBOutlet weak var modalView: UIView!
+    @IBOutlet weak var modalTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    var confirm: Bool = false
     var coins = SharingManager.sharedInstance.lifetimeScore
+    var cost: Int = 0
+    var place: Int = 0
+    var buyButton: UIButton? = nil
+    var coin: UIImageView? = nil
+    var itemTitle: String = ""
     
     @IBOutlet weak var startOver: UIImageView!
     override func viewDidLoad() {
@@ -42,6 +52,14 @@ class StoreViewController: UIViewController {
         }
     }
     
+    @IBAction func confirmPressed(_ sender: Any) {
+        confirm = true
+        hideModal()
+    }
+    @IBAction func cancelPressed(_ sender: Any) {
+        hideModal()
+    }
+    
     // Change display to use button
     func itemAlreadyPurchased(buyButton: UIButton, coin: UIImageView) {
         buyButton.setTitle("use", for: .normal)
@@ -54,7 +72,7 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var firstCoin: UIImageView!
     @IBAction func firstBuy(_ sender: Any) {
         if SharingManager.sharedInstance.itemStates[0] == "inStore" {
-            purchaseItem(cost: 1, place: 0, buyButton: firstBuyButton, coin: firstCoin)
+            purchaseItem(price: 1, num: 0, button: firstBuyButton, image: firstCoin, title: "shades")
         }
         else {
             print("use")
@@ -64,7 +82,7 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var secondBuyButton: UIButton!
     @IBAction func secondBuy(_ sender: Any) {
         if SharingManager.sharedInstance.itemStates[1] == "inStore" {
-            purchaseItem(cost: 2000, place: 1, buyButton: secondBuyButton, coin: secondCoin)
+            purchaseItem(price: 2000, num: 1, button: secondBuyButton, image: secondCoin, title: "chain")
         }
         else {
             print("use")
@@ -74,7 +92,7 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var thirdBuyButton: UIButton!
     @IBAction func thirdBuy(_ sender: Any) {
         if SharingManager.sharedInstance.itemStates[3] == "inStore" {
-            purchaseItem(cost: 5000, place: 2, buyButton: thirdBuyButton, coin: thirdCoin)
+            purchaseItem(price: 5000, num: 2, button: thirdBuyButton, image: thirdCoin, title: "monocle")
         }
         else {
             print("use")
@@ -84,7 +102,7 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var fourthBuyButton: UIButton!
     @IBAction func fourthBuy(_ sender: Any) {
         if SharingManager.sharedInstance.itemStates[3] == "inStore" {
-            purchaseItem(cost: 10000, place: 3, buyButton: fourthBuyButton, coin: fourthCoin)
+            purchaseItem(price: 10000, num: 3, button: fourthBuyButton, image: fourthCoin, title: "mustache")
         }
         else {
             print("use")
@@ -94,7 +112,7 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var fifthBuyButton: UIButton!
     @IBAction func fifthBuy(_ sender: Any) {
         if SharingManager.sharedInstance.itemStates[4] == "inStore" {
-            purchaseItem(cost: 100000, place: 4, buyButton: fifthBuyButton, coin: fifthCoin)
+            purchaseItem(price: 100000, num: 4, button: fifthBuyButton, image: fifthCoin, title: "?????")
         }
         else {
             print("use")
@@ -109,16 +127,59 @@ class StoreViewController: UIViewController {
     }
     
     // Try to buy something
-    func purchaseItem(cost: Int, place: Int, buyButton: UIButton, coin: UIImageView) {
+    func purchaseItem(price: Int, num: Int, button: UIButton, image: UIImageView, title: String) {
+        cost = price
+        place = num
+        buyButton = button
+        coin = image
+        itemTitle = title
+        
         if cost <= coins {
+            modalView.layer.cornerRadius = 15
+            confirmButton.layer.cornerRadius = 20
+            messageLabel.text = "Buy \(itemTitle) for \(cost) coins?"
+            showModal()
+        }
+        else {
+            // popup not enough coins - buy more?
+//            modalView.backgroundColor = UIColor(colorLiteralRed: 0/255, green: 93/255, blue: 172/255, alpha: 1)
+            modalView.layer.cornerRadius = 15
+            confirmButton.layer.cornerRadius = 20
+            messageLabel.text = "Oops! You don't have enough coins. Would you like to buy more?"
+            showModal()
+        }
+    }
+    
+    func showModal() {
+        modalTopConstraint.constant += self.view.bounds.height
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.modalView.isHidden = false
+            self.modalTopConstraint.constant -= self.view.bounds.height
+            
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func hideModal() {
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.modalTopConstraint.constant += self.view.bounds.height
+            self.view.layoutIfNeeded()
+        }, completion: { (finished: Bool) in
+            if finished {
+                self.modalView.isHidden = true
+                self.modalTopConstraint.constant -= self.view.bounds.height
+            }
+        })
+        if confirm == true && coins >= cost {
             coins -= cost
             currentCoins.text = "\(coins)"
             SharingManager.sharedInstance.lifetimeScore = coins
             SharingManager.sharedInstance.itemStates[place] = "inCloset"
-            itemAlreadyPurchased(buyButton: buyButton, coin: coin)
-        }
-        else {
-            // popup not enough coins - buy more?
+            itemAlreadyPurchased(buyButton: buyButton!, coin: coin!)
         }
     }
     
